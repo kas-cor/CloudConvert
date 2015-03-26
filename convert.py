8b18023cfa672ac83e2892b0e7bf29c22c2ca35c
kas-cor <kascorp@gmail.com>
2015-03-26 19:02:25 +0300

    Fix - Two and more argumets

--- convert.py
+++ convert.py
@@ -27,6 +27,18 @@ def get_outputformat(f):
             break
     return {'filename': f, 'from': ext, 'to': outext[sel - 1]}
 
+def convert(apikey, inputfile, outputfile):
+    process = CloudConvert.ConversionProcess(apikey)
+    process.init(inputfile, outputfile)
+    print("start convert - " + inputfile + " to " + outputfile)
+    process.start()
+    print("waiting...")
+    process.wait_for_completion()
+    print("Saving")
+    process.save()
+    print("Open")
+    os.system("start " + outputfile)
+
 task = []
 for f in sys.argv[1:]:
     if os.path.exists(f):
@@ -34,16 +46,4 @@ for f in sys.argv[1:]:
 
 for t in task:
     if CloudConvert.CloudConvert.is_possible(t['from'], t['to']):
-        inputfile = t['filename']
-        outputfile = t['filename'] + "." + t['to']
-        process = CloudConvert.ConversionProcess(apikey)
-        process.init(inputfile, outputfile)
-        print("start convert - " + inputfile + " to " + outputfile)
-        process.start()
-        print("waiting...")
-        process.wait_for_completion()
-        print("Saving")
-        process.save()
-        print("Open")
-        os.system("start " + outputfile)
-        
+        convert(apikey, t['filename'], t['filename'] + "." + t['to'])
