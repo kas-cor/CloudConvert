72ba1f986b7ffac398fb5c80ef7bb49e3387b999
kas-cor <kascorp@gmail.com>
2015-04-15 15:52:51 +0300

    PEP8

--- convert.py
+++ convert.py
@@ -8,6 +8,7 @@ curdir = os.path.abspath(os.path.dirname(sys.argv[0]));
 
 apikey = open(curdir + "/apikey.txt", "r").read().strip()
 
+
 def get_outputformat(f):
     ext = f.split(".")[-1]
     print("File: " + f)
@@ -29,6 +30,7 @@ def get_outputformat(f):
             break
     return {'filename': f, 'from': ext, 'to': outext[sel - 1]}
 
+
 def convert(apikey, inputfile, outputfile):
     process = CloudConvert.ConversionProcess(apikey)
     process.init(inputfile, outputfile)
@@ -40,6 +42,7 @@ def convert(apikey, inputfile, outputfile):
     process.save()
     print("Open " + outputfile)
     os.system("start " + outputfile)
+
 
 task = []
 for f in sys.argv[1:]:
